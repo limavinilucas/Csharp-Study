98ee87a0b65be8297b07d47699dcb67a0573b496
Lucas Vinicius <137515842+limavinilucas@users.noreply.github.com>
2024-05-13 00:21:40 -0300

    v01 - Upload Base Files
    - Upload the base files of the repository.

---
+++ BoardDice.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("\t-= Welcome to Dice's =-\t");
+Console.WriteLine("> Please, press 'Enter' to continue... \t");
+Console.ReadLine();
+int dice = new Random().Next(200);
+Console.WriteLine($"Dice's face: {dice}");
